453392c71a7cbab56a95c414c140132be1d3995b
Luke Sands <luke.sands@d3code.com.au>
2022-12-11 01:58:15 +1100

    Updates

--- etherscan/token.go
+++ etherscan/token.go
@@ -26,3 +26,32 @@ func (c *client) GetTokenInfoByContractAddress(contractAddress string) (*TokenIn
 
     return &etherscanResponse, resBody, nil
 }
+
+func (c *client) GetTokenTransfersByContractAddress(contractAddress string, address string,
+    page string, offset string, startBlock string, endBlock string, sort string) (*TokenInfoByContractAddress, []byte, error) {
+
+    values := url.Values{}
+    values.Add("module", "account")
+    values.Add("action", "tokentx")
+    values.Add("contractaddress", contractAddress)
+    values.Add("address", address)
+    values.Add("page", page)
+    values.Add("offset", offset)
+    values.Add("startblock", startBlock)
+    values.Add("endblock", endBlock)
+    values.Add("sort", sort)
+
+    resBody, err := doGetRequest(values, c)
+    if err != nil {
+        return nil, resBody, err
+    }
+
+    var etherscanResponse TokenInfoByContractAddress
+
+    unmarshalError := json.Unmarshal(resBody, &etherscanResponse)
+    if unmarshalError != nil {
+        return nil, resBody, unmarshalError
+    }
+
+    return &etherscanResponse, resBody, nil
+}
