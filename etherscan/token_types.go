6520a696857aa03b776f8be1c58d79561afbf56b
Luke Sands <luke.sands@d3code.com.au>
2022-12-18 11:26:06 +1100

    Updates

--- etherscan/token_types.go
+++ etherscan/token_types.go
@@ -1,29 +1,31 @@
 package etherscan
 
+import "time"
+
 type TokenTransactions struct {
     Status  string `json:"status"`
     Message string `json:"message"`
     Result  []struct {
-        BlockNumber       string `json:"blockNumber"`
-        TimeStamp         string `json:"timeStamp"`
-        Hash              string `json:"hash"`
-        Nonce             string `json:"nonce"`
-        BlockHash         string `json:"blockHash"`
-        TransactionIndex  string `json:"transactionIndex"`
-        From              string `json:"from"`
-        To                string `json:"to"`
-        Value             string `json:"value"`
-        Gas               string `json:"gas"`
-        GasPrice          string `json:"gasPrice"`
-        IsError           string `json:"isError"`
-        TxreceiptStatus   string `json:"txreceipt_status"`
-        Input             string `json:"input"`
-        ContractAddress   string `json:"contractAddress"`
-        CumulativeGasUsed string `json:"cumulativeGasUsed"`
-        GasUsed           string `json:"gasUsed"`
-        Confirmations     string `json:"confirmations"`
-        MethodId          string `json:"methodId"`
-        FunctionName      string `json:"functionName"`
+        BlockNumber       int       `json:"blockNumber"`
+        TimeStamp         time.Time `json:"timeStamp"`
+        Hash              string    `json:"hash"`
+        Nonce             string    `json:"nonce"`
+        BlockHash         string    `json:"blockHash"`
+        TransactionIndex  string    `json:"transactionIndex"`
+        From              string    `json:"from"`
+        To                string    `json:"to"`
+        Value             string    `json:"value"`
+        Gas               string    `json:"gas"`
+        GasPrice          string    `json:"gasPrice"`
+        IsError           bool      `json:"isError"`
+        TxreceiptStatus   string    `json:"txreceipt_status"`
+        Input             string    `json:"input"`
+        ContractAddress   string    `json:"contractAddress"`
+        CumulativeGasUsed string    `json:"cumulativeGasUsed"`
+        GasUsed           string    `json:"gasUsed"`
+        Confirmations     int       `json:"confirmations"`
+        MethodId          string    `json:"methodId"`
+        FunctionName      string    `json:"functionName"`
     } `json:"result"`
 }
 
